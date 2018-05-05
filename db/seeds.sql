e53cb905bb6ee541a5177389b471fe6401de6fc5
unknown <vleague2@gmail.com>
2018-05-04 22:57:20 -0400

    set up sql files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,19 @@
 USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES (Hawaiian, 0);
+VALUES ('Hawaiian', 0);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES (Bacon Cheeseburger, 1);
+VALUES ('Bacon Cheeseburger', 1);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES (Homewrecker, 0);
+VALUES ('Homewrecker', 0);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES (California, 0);
+VALUES ('California', 0);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES (Turkey Burger, 0);
+VALUES ('Turkey Burger', 0);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES(Cosmic, 0);
+VALUES('Cosmic', 0);
