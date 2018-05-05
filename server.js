00c89a1b2848f07958a6a5f7e684b7e4fa0edfe0
unknown <vleague2@gmail.com>
2018-05-05 15:38:14 -0400

    set up server

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
 // REQUIRE MODULES 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// SET UP SERVER PORT
+const PORT = process.env.PORT || 8080;
+
+// SET UP EXPRESS
+const app = express();
+
+// SET STATIC CONTENT FOLDER
+app.use(express.static('public'));
+
+// SET UP BODY PARSER
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// SET UP HANDLEBARS
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// IMPORT ROUTES
+const routes = require('./controllers/burgers_controller');
+app.use(routes);
+
+// START SERVER
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost: " + PORT);
+})
