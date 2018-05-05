4a2df785dd9ba790067b31c1c8d7c0e717806f3d
unknown <vleague2@gmail.com>
2018-05-04 23:01:27 -0400

    set up connection

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// REQUIRE MYSQL MODULE
+const mysql = require('mysql');
+
+// CREATE OBJECT TO HOLD CONNECTION INFO FOR MYSQL
+let connection = mysql.createConnection({
+    port: 3306,
+    host: '127.0.0.1',
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
+
+// CONNECT TO THE DATABASE
+connection.connect(err => {
+    if (err) {
+
+        // IF ERROR, REPORT ERROR
+        console.log("error connecting: " + err.stack);
+        return;
+    }
+
+    // IF SUCCESSFUL, REPORT CONNECTION ID
+    console.log("connected as id " + connection.threadId);
+})
+
+// EXPORT THE CONNECTION
+module.exports = connection;
