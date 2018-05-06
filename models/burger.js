a27f8e4c384569cff77630f09639eca2a96ccecf
unknown <vleague2@gmail.com>
2018-05-06 10:34:15 -0400

    commented burger.js

--- models/burger.js
+++ models/burger.js
@@ -3,20 +3,36 @@ const orm = require('../config/orm');
 
 // CREATE BURGER OBJECT
 let burger = {
+
+    // select all function
     all: function(callback) {
+
+        // which calls the selectAll in the orm object and passes it a variable
         orm.selectAll("burgers", (res) => {
+
+            // send the response to the callback
             callback(res);
         })
     },
 
+    // update function, which takes a lot of parameters
     update: function(col, val, whereCol, whereVal, callback) {
+
+        // call the orm updateOne function and pass it the parameters it needs
         orm.updateOne("burgers", col, val, whereCol, whereVal, (res) => {
+
+            // send the response to the callback
             callback(res);
         })
     },
 
+    // insert function
     insert: function(name, callback) {
+
+        // calls the orm insertOne function and passes it the parameters it needs
         orm.insertOne("burgers", name, (res) => {
+
+            // send the response to the callback
             callback(res);
         })
     }
